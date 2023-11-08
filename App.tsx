67f1d0f6fe9ab99e621e56fa69d0f3fdd3274377
maritocuate <mario300@gmail.com>
2023-11-08 16:51:44 -0300

    chore: sections

--- App.tsx
+++ App.tsx
@@ -4,8 +4,10 @@ import { StyleSheet, Text, View } from 'react-native'
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello World</Text>
-      <StatusBar style="auto" />
+      <View style={styles.tasksWrapper}>
+        <Text style={styles.sectionTitle}>Todays taks</Text>
+        <View style={styles.items}>{/* <Tasks /> */}</View>
+      </View>
     </View>
   )
 }
@@ -16,5 +18,16 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  tasksWrapper: {
+    paddingTop: 80,
+    paddingHorizontal: 20,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: 'bold',
+  },
+  items: {
+    marginTop: 30,
   },
 })
